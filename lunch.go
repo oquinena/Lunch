d5db507c0d6ee0b5ff7cebf41e6a0cf5e65a1dac
Daniel de Oquiñena <daniel.oquinena@safespring.com>
2023-04-20 17:55:18 +0200

    testing

--- lunch.go
+++ lunch.go
@@ -1,3 +1,4 @@
+// testing testing
 package main
 
 import (
@@ -8,7 +9,9 @@ import (
 	"time"
 
 	c "github.com/TreyBastian/colourize"
+	"github.com/getlantern/systray"
 	"github.com/mmcdole/gofeed"
+	"github.com/oquinena/lunch/icon"
 )
 
 type lunchalt struct {
@@ -16,10 +19,90 @@ type lunchalt struct {
 	veg    string
 }
 
+func lunch(skola string) (lunchalt, error) {
+	var lv lunchalt
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL("https://skolmaten.se/" + skola + "/rss/days/")
+	if err != nil {
+		fmt.Errorf("Fel vid hämtning av lunchalternativ")
+		return lv, err
+	}
+	l := feed.Items[0].Description
+	lunch := strings.Split(l, "<br/>")
+	lv.vanlig, lv.veg = lunch[0], lunch[1]
+
+	return lv, err
+}
+
+func runTray() {
+   	birka, err := lunch("birkaskolan")
+    if err != nil {
+        fmt.Println(err)
+    }
+	ekebyhov, err := lunch("ekebyhovskolan")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    systray.SetIcon(icon.Data)
+    systray.SetTitle("Lunch")
+    systray.SetTooltip("Lunch")
+    systray.AddMenuItem("Birkaskolan:", "Birka")
+    mBirkaVanlig := systray.AddMenuItem(fmt.Sprintf("Vanlig: %s", birka.vanlig), "Birka")
+    mBirkaVeg := systray.AddMenuItem(fmt.Sprintf("Vegetarisk: %s", birka.veg), "Birka")
+    systray.AddMenuItem("Ekebyhovskolan:", "Ekebyhov")
+    mEkebyhovVanlig := systray.AddMenuItem(fmt.Sprintf("Vanlig: %s", ekebyhov.vanlig), "Ekebyhov")
+    mEkebyhovVeg := systray.AddMenuItem(fmt.Sprintf("Vegetarisk: %s", ekebyhov.veg), "Ekebyhov")
+    mQuit := systray.AddMenuItem("Quit", "Quit the app")
+    go func() {
+        for {
+            select {
+            case <-mBirkaVanlig.ClickedCh:
+                fmt.Println("Birka vanlig")
+            case <-mBirkaVeg.ClickedCh:
+                fmt.Println("Birka veg")
+            case <-mEkebyhovVanlig.ClickedCh:
+                fmt.Println("Ekebyhov vanlig")
+            case <-mEkebyhovVeg.ClickedCh:
+                fmt.Println("Ekebyhov veg")
+            case <-mQuit.ClickedCh:
+                systray.Quit()
+            }
+        }
+    }()
+
+    ticker := time.NewTicker(time.Minute * 5)
+    tQuit := make(chan bool)
+    go func() {
+        for {
+            select {
+            case <-ticker.C:
+                birka, err := lunch("birkaskolan")
+                if err != nil {
+                    fmt.Println(err)
+                }
+                ekebyhov, err := lunch("ekebyhovskolan")
+                if err != nil {
+                    fmt.Println(err)
+                }
+                mBirkaVanlig.SetTitle(fmt.Sprintf("Vanlig: %s", birka.vanlig))
+                mBirkaVeg.SetTitle(fmt.Sprintf("Vegetarisk: %s", birka.veg))
+                mEkebyhovVanlig.SetTitle(fmt.Sprintf("Vanlig: %s", ekebyhov.vanlig))
+                mEkebyhovVeg.SetTitle(fmt.Sprintf("Vegetarisk: %s", ekebyhov.veg))
+            case <-tQuit:
+                ticker.Stop()
+                return
+            }
+        }
+    }()
+}
+    
+
 func main() {
 	all := flag.Bool("a", false, "Visar även vegetariska rätter")
 	b := flag.Bool("b", false, "Endast Birkaskolans lunch")
 	e := flag.Bool("e", false, "Visa endast Ekebyhovskolans lunch")
+    tray := flag.Bool("t", false, "Visa lunchen i systemfältet")
 	flag.Parse()
 
 	var dagar = map[string]time.Weekday{
@@ -38,8 +121,22 @@ func main() {
 		os.Exit(0)
 	}
 
-	birka, _ := lunch("birkaskolan")
-	ekebyhov, _ := lunch("ekebyhovskolan")
+	birka, err := lunch("birkaskolan")
+    if err != nil {
+        fmt.Println(err)
+    }
+	ekebyhov, err := lunch("ekebyhovskolan")
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    if *tray {
+        stop := func() {
+            now := time.Now()
+            fmt.Println("Bye bye", now)
+        }
+        systray.Run(runTray, stop)
+    }
 
 	if *all {
 		fmt.Printf(c.Colourize("Birkaskolan:", c.Green))
@@ -56,17 +153,3 @@ func main() {
 	}
 }
 
-func lunch(skola string) (lunchalt, error) {
-	var lv lunchalt
-	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL("https://skolmaten.se/" + skola + "/rss/days/")
-	if err != nil {
-		fmt.Errorf("Fel vid hämtning av lunchalternativ")
-		return lv, err
-	}
-	l := feed.Items[0].Description
-	lunch := strings.Split(l, "<br/>")
-	lv.vanlig, lv.veg = lunch[0], lunch[1]
-
-	return lv, err
-}
